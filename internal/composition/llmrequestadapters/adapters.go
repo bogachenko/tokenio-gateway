75d3dd087cef8154bc4c1b2895b23e3c4d0cca09
Ivan <ivanbogachenco@gmail.com>
2026-06-21 23:05:27 +0300

    Move retry waiter to LLM request adapters

--- internal/composition/llmrequestadapters/adapters.go
+++ internal/composition/llmrequestadapters/adapters.go
@@ -3,6 +3,7 @@ package llmrequestadapters
 import (
 	"context"
 	"errors"
+	"time"
 
 	authenticateapp "github.com/bogachenko/tokenio-gateway/internal/application/authenticate"
 	billingapp "github.com/bogachenko/tokenio-gateway/internal/application/billing"
@@ -255,6 +256,30 @@ func NewLLMRequestForwardingExecutor(
 	maxResponseBodyBytes int64,
 ) (*llmrequest.LLMRequestForwardingExecutor, error) {
 	return llmrequest.NewLLMRequestForwardingExecutor(secrets, factory, maxResponseBodyBytes)
+}
+
+type ContextRetryWaiter struct{}
+
+func (ContextRetryWaiter) Wait(
+	ctx context.Context,
+	delay time.Duration,
+) error {
+	if ctx == nil || delay < 0 {
+		return llmrequest.ErrInvalidInput
+	}
+	if delay == 0 {
+		return ctx.Err()
+	}
+
+	timer := time.NewTimer(delay)
+	defer timer.Stop()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-timer.C:
+		return nil
+	}
 }
 
 func copyInt64Pointer(value *int64) *int64 {
