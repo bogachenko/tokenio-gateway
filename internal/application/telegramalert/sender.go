88da5430ac890ddaffe49d2c4bba4330de104a33
Ivan <ivanbogachenco@gmail.com>
2026-06-21 18:34:01 +0300

    Alias Telegram delivery contract in application

--- internal/application/telegramalert/sender.go
+++ internal/application/telegramalert/sender.go
@@ -1,26 +1,15 @@
 package telegramalert
 
-import "context"
+import "github.com/bogachenko/tokenio-gateway/internal/ports/telegramalertdelivery"
 
-type MessageDeliveryOutcome string
+type MessageDeliveryOutcome = telegramalertdelivery.MessageDeliveryOutcome
 
 const (
-	MessageDeliveryOutcomeNotSent          MessageDeliveryOutcome = "not_sent"
-	MessageDeliveryOutcomeSentNoResponse   MessageDeliveryOutcome = "sent_no_response"
-	MessageDeliveryOutcomeResponseReceived MessageDeliveryOutcome = "response_received"
+	MessageDeliveryOutcomeNotSent          = telegramalertdelivery.MessageDeliveryOutcomeNotSent
+	MessageDeliveryOutcomeSentNoResponse   = telegramalertdelivery.MessageDeliveryOutcomeSentNoResponse
+	MessageDeliveryOutcomeResponseReceived = telegramalertdelivery.MessageDeliveryOutcomeResponseReceived
 )
 
-type MessageDeliveryResult struct {
-	Outcome           MessageDeliveryOutcome
-	TelegramMessageID string
-}
+type MessageDeliveryResult = telegramalertdelivery.MessageDeliveryResult
 
-// MessageSender delivers an already composed Telegram message.
-// Outcome deterministically describes how far the external side effect reached.
-// The sender does not mutate persisted alert or attempt lifecycle state.
-type MessageSender interface {
-	SendMessage(
-		context.Context,
-		string,
-	) (MessageDeliveryResult, error)
-}
+type MessageSender = telegramalertdelivery.MessageSender
